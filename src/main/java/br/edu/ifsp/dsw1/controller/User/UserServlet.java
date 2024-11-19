35f2f182cb50dd99b56ffcb402bfb1b2b6b6c72b
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2024-11-19 17:54:03 -0300

    implementando totens

--- src/main/java/br/edu/ifsp/dsw1/controller/User/UserServlet.java
+++ src/main/java/br/edu/ifsp/dsw1/controller/User/UserServlet.java
@@ -1,13 +1,22 @@
 package br.edu.ifsp.dsw1.controller.User;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import br.edu.ifsp.dsw1.controller.command.Command;
+import br.edu.ifsp.dsw1.model.entity.FlightData;
+import br.edu.ifsp.dsw1.model.entity.FlightDataCollection;
+import br.edu.ifsp.dsw1.model.entity.FlightDataCollectionSingleton;
+import br.edu.ifsp.dsw1.model.flightstates.Arriving;
+import br.edu.ifsp.dsw1.model.flightstates.Boarding;
+import br.edu.ifsp.dsw1.model.flightstates.TakingOff;
+import br.edu.ifsp.dsw1.model.flightstates.TookOff;
 
 @WebServlet("/user.do")
 public class UserServlet extends HttpServlet{
@@ -18,12 +27,88 @@ public class UserServlet extends HttpServlet{
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		processRequest(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		processRequest(request, response);
 	}
 
-
+	
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+		String view = null;
+		
+		if(action.equals("getSalaDesembarque")) {
+			view = handleSalaDesembarque(request, response);
+		}else if(action.equals("getSalaEmbarque")) {
+			view = handleSalaEmbarque(request, response);
+		}else if(action.equals("getHall1")) {
+			view = handleHall1(request, response);
+		}else if(action.equals("getHall2")) {
+			view = handleHall2(request, response);
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+		dispatcher.forward(request, response);
+		
+	}
+	
+	protected String handleSalaDesembarque(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		FlightDataCollection flightCollection = FlightDataCollectionSingleton.getInstance();
+		
+		//criando uma lista que recebe apenas objetos FlighData com estado Arriving
+		List<FlightData> flightsArriving = flightCollection.getAllFligthts().stream()
+													.filter(flight -> flight.getState() instanceof Arriving)
+													.toList();
+		
+		request.setAttribute("flightsArriving", flightsArriving);
+		
+		return "salaDesembarque.jsp";
+	}
+	
+	protected String handleSalaEmbarque(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		FlightDataCollection flightCollection = FlightDataCollectionSingleton.getInstance();
+		
+		//criando uma lista que recebe apenas objetos FlighData com estado Boarding
+		List<FlightData> flightsArriving = flightCollection.getAllFligthts().stream()
+													.filter(flight -> flight.getState() instanceof Boarding)
+													.toList();
+		
+		request.setAttribute("flightsArriving", flightsArriving);
+		
+		return "salaEmbarque.jsp";
+	}
+	
+	protected String handleHall1(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		FlightDataCollection flightCollection = FlightDataCollectionSingleton.getInstance();
+		
+		//criando uma lista que recebe apenas objetos FlighData com estado TakingOff
+		List<FlightData> flightsArriving = flightCollection.getAllFligthts().stream()
+													.filter(flight -> flight.getState() instanceof TakingOff)
+													.toList();
+		
+		request.setAttribute("flightsArriving", flightsArriving);
+		
+		return "hall1.jsp";
+	}
+	
+	protected String handleHall2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		FlightDataCollection flightCollection = FlightDataCollectionSingleton.getInstance();
+		
+		//criando uma lista que recebe apenas objetos FlighData com estado TookOff
+		List<FlightData> flightsArriving = flightCollection.getAllFligthts().stream()
+													.filter(flight -> flight.getState() instanceof TookOff)
+													.toList();
+		
+		request.setAttribute("flightsArriving", flightsArriving);
+		
+		return "hall2.jsp";
+	}
+	
 }
--- src/main/webapp/hall2.jsp
+++ src/main/webapp/hall2.jsp
@@ -6,7 +6,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Cadastrado com Sucesso</title>
+<title>Hall 2</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <style>
 	.alert-success{
@@ -14,29 +14,51 @@
 		flex-direction: column;
 		align-items: center;
 	}
+	.container{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 </style>
 </head>
-
-<%
-Long numeroVoo = Long.parseLong(request.getParameter("text_numero_voo"));
-String companhia = request.getParameter("text_companhia");
-String dataHora = request.getParameter("text_data_hora");
-%>
-
 <body>
-
-	<div class="alert alert-success" role="alert">
-		  <h4 class="alert-heading">Voo Cadastrado!</h4>
-		  <h5 class="alert-heading">Informações:</h5>
-		  <p>Número: <%= numeroVoo%></p>
-		  <p>Companhia: <%= companhia%></p>
-		  <p>Data/Hora: <%= dataHora%></p>
-		  <a class="btn btn-primary btn-lg" href="login.do?action=getAdm" role="button">Administração</a>
+	<%
+		var flightsArriving = (List<FlightData>) request.getAttribute("flightsArriving");
+	%>
+	<%if(flightsArriving != null && !flightsArriving.isEmpty()){%>
+		<table class="table table-dark">
+			  <thead>
+				    <tr>
+					      <th scope="col">Número</th>
+					      <th scope="col">Companhia</th>
+					      <th scope="col">Data/Hora</th>
+					      <th scope="col">Estado</th>
+				    </tr>
+			  </thead>
+			  <tbody>
+			  		<%for(var flights : flightsArriving){%>
+				    <tr>
+					      <th scope="row"><%= flights.getFlightNumber()%></th>
+					      <td><%= flights.getCompany()%></td>
+					      <td><%= flights.getTime() %></td>
+					      <td><%= flights.getState().getClass().getSimpleName() %></td>
+				    </tr>
+				    <%};%>
+			  </tbody>
+		</table>
+	<%}else{ %>
+		<div class="alert alert-success" role="alert">
+		  <h4 class="alert-heading">Nenhum voo cadastrado!</h4>
+	    </div>
+	<%}; %>
+	
+	<div class="container">
+		<a class="btn btn-primary btn-lg" href="login.do?action=getIndex" role="button">Página Inicial</a>
 	</div>
 	
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+	
 </body>
 </html>
